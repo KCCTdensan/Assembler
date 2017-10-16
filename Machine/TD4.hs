e821ba2dfafc7b1562c181ac2931544291bde02f
kcct-dk <r115314@g.kobe-kosen.ac.jp>
2017-10-16 18:34:25 +0900

    整形
    形已。
    手直しの要有り。

---
+++ Machine/TD4.hs
@@ -0,0 +1,17 @@
+module Machine.TD4 (
+	translate
+) where
+
+translate :: String -> String
+translate "mov a, im" = "0011im"
+translate "mov b, im" = "0111im"
+translate "mov a, b" = "00010000"
+translate "mov b, a" = "01000000"
+translate "add a, im" = "0000im"
+translate "add b, im" = "0101im"
+translate "in a" = "00100000"
+translate "in b" = "01100000"
+translate "out im" = "1011im"
+translate "out b" = "10010000"
+translate "jmp im" = "1111im"
+translate "jnc im" = "1110im"
--- Main.hs
+++ Main.hs
@@ -1,9 +1,25 @@
 import System.Environment
 import System.IO
+import Text.Regex
+import Data.Char
+
+import qualified Machine.TD4 as TD4
+
+data Machine = TD4 | Type1
 
 main = do
-	file <- head <$> getArgs
-	lines <$> readFile file >>= print
+	-- [要修正] 選項解析処理及錯誤時之処理
+	inputFile <- head <$> getArgs
+	let machine = TD4
+	-- 此処迄
+
+	let outputFile = head (splitRegex (mkRegex "[.]+") inputFile) ++ ".bin"
+	ops <- lines <$> readFile inputFile
+	writeFile outputFile (unlines $ assemble machine ops) -- [要修正] 例外処理追加
 
 help :: String
-help = ""
+help = "" -- [要修正]
+
+assemble :: Machine -> [String] -> [String]
+assemble TD4 ops = map TD4.translate $ map (map toLower) ops
+assemble Type1 ops = ["Sorry, this machine is unsupported yet."]
