dfdc13374219ac6a1efd591533e6b6c63a83650e
kcct-dk <r115314@g.kobe-kosen.ac.jp>
2017-10-17 19:00:59 +0900

    更新

--- Machine/TD4.hs
+++ Machine/TD4.hs
@@ -4,7 +4,13 @@ module Machine.TD4 (
 
 import Text.Regex
 
-import Utils.Base as Base
+import Utils.Numeric
+
+width :: Int
+width = 8
+
+toBin :: String -> String
+toBin = .toBin 2
 
 -- [要修正]: 例外処理之追加
 assemble :: String -> String
@@ -16,19 +22,19 @@ assemble op =
 		"mov"	->	case operand !! 0 of
 						"a,"	->	case operand !! 1 of
 										"b"	-> "00010000"
-									 	im	-> "0011" ++ dec2bin im
+									 	im	-> "0011" ++ toBin im
 						"b,"	->	case operand !! 1 of
 										"a"	-> "01000000"
-										im	-> "0111" ++ dec2bin im
+										im	-> "0111" ++ toBin im
 		"add"	->	case operand !! 0 of
-						"a,"	-> "0000" ++ dec2bin (operand !! 1)
-						"b,"	-> "0101" ++ dec2bin (operand !! 1)
+						"a,"	-> "0000" ++ toBin (operand !! 1)
+						"b,"	-> "0101" ++ toBin (operand !! 1)
 		"in"		->	case operand !! 0 of
 						"a"	-> "00100000"
 						"b"	-> "01100000"
 		"out"	->	case operand !! 0 of
 						"b"	-> "10010000"
-						im	-> "1011" ++ dec2bin im
-		"jmp"	-> "1111" ++ dec2bin (operand !! 0)
-		"jnc"	-> "1110" ++ dec2bin (operand !! 0)
+						im	-> "1011" ++ toBin im
+		"jmp"	-> "1111" ++ toBin (operand !! 0)
+		"jnc"	-> "1110" ++ toBin (operand !! 0)
 		_		-> error $ "錯誤 (存在為無い命令): " ++ op
--- Utils/Base.hs
+++ Utils/Base.hs
@@ -1,7 +1,19 @@
-module Utils.Base (
-	dec2bin
+module Utils.Numeric (
+	toBin
 ) where
 
--- [要修正]
-dec2bin :: String -> String
-dec2bin x = "Im: " ++ x
+import Numeric
+import Data.Char
+
+showBin :: String -> String
+showBin x = showIntAtBase 2 intToDigit (read x) ""
+
+padding :: Int -> String -> String
+padding n x =
+	let diff = n - length x
+	in if n < length x
+		then error $ "錯誤 (表現可能な範囲を超過): " ++ x
+		else ['0' | x <- [1..diff]] ++ x
+
+toBin :: Int -> String -> String
+toBin n x = padding n $ showBin x
