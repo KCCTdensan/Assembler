fd739bc4cc0e59e24ac29f9b58fe03e4f2f5b78d
kcct-dk <31849967+kcct-dk@users.noreply.github.com>
2017-10-21 21:00:38 +0900

    help執筆

--- Main.hs
+++ Main.hs
@@ -17,9 +17,28 @@ main = do
 	ops <- lines <$> readFile inputFile
 	writeFile outputFile (unlines $ assemble machine ops) -- [要修正] 例外処理追加
 
-help :: String
-help = "" -- [要修正]
-
 assemble :: Machine -> [String] -> [String]
 assemble TD4 ops = map TD4.assemble $ map (map toLower) ops
-assemble Type1 ops = ["Sorry, this machine is unsupported yet."]
+assemble Type1 ops = ["Sorry, this machine is unsupportedyet."]
+
+help :: String
+help = "---------- Assembler 第0.0.1版 ----------\n" ++
+	"\n" ++
+	"    [説明]\n" ++
+	"        アセンブル及び逆アセンブルを行う算譜です。\n" ++
+	"\n" ++
+	"    [使い方]\n" ++
+	"        asm -[選項] (ファイル名)\n" ++
+	"\n" ++
+	"        <選項一覧>\n" ++
+	"            t (計算機名): 対象を指定する\n" ++
+	"                計算機名: td4, ...\n" ++
+	"            r: 逆アセンブルを行う\n" ++
+	"\n" ++
+	"        <例>\n" ++
+	"            asm -t td4 program.asm\n" ++
+	"            asm -t td4 -r program.bin\n" ++
+	"\n" ++
+	"    [情報]\n" ++
+	"        制作元: 神戸高専 電子計算機部 計算機製作プロジェクト\n" ++
+	"        製作場: https://github.com/orgs/KCCTdensan/teams/team-1/members"
