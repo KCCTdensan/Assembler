02281c63fff510bc34e216dcefcde5ccc2c3e9be
kcct-dk <31849967+kcct-dk@users.noreply.github.com>
2017-11-12 21:22:16 +0900

    up

--- Main.hs
+++ Main.hs
@@ -9,42 +9,41 @@ import Data.IORef
 import Control.Monad
 import Text.Regex
 
-import System.Console.CmdArgs
+import System.Console.CmdArgs hiding (Mode)
 import qualified Machine.TD4 as TD4
 
-data Machine = NoSelM | TD4 | Type1 | Type2 deriving (Data, Eq, Show)
-data Direction = NoSelD | ASM | DisASM deriving (Data, Eq, Show)
+data Computer = NoSelC | TD4 | Type1 | Type2 deriving (Data, Eq, Show)
+data Mode = NoSelD | ASM | DisASM deriving (Data, Eq, Show)
 data Verbose = OFF | ON deriving (Data, Show)
 
 data CmdOpt = CmdOpt {
-	machine :: Machine,
-	direction :: Direction,
+	computer :: Computer,
+	mode :: Mode,
 	file :: FilePath,
---	output :: FilePath,
+	output :: FilePath,
 	verbose :: Verbose
 } deriving (Data, Show)
 
 cmdOpt :: CmdOpt
 cmdOpt = CmdOpt
 	{
-		machine	=	NoSelM
+		computer =	NoSelC
 				&=	typ "{TD4 | Type1 | Type2}"
 				&=	help "対象之計算機名"
 				&=	groupname "換符ニ関為ル選項",
-		direction	=	NoSelD
+		mode	=	NoSelD
 				&=	typ "{ASM | DisASM}"
 				&=	help "換符之方向 (具意符→機械符: ASM, 具意符←機械符: DisASM)"
 				&=	groupname "換符ニ関為ル選項",
 		file 		=	def
 				&=	argPos 0
 				&=	typ "算譜名",
--- [要修正]: 出力先選択機能之追加
-{-		output	=	def
+		output	=	def
 				&=	opt ""
-				&=	typ " 算譜名.変換先之拡張子 | _"
-				&=	help "出力先之指定 (指定無キ時、入力為レシ算譜名之拡張子ヲ変更為タ物ト成ル。)"
+				&=	typ " (譜之本体名).{意符: asm, 械符: bin} | (選項値)"
+				&=	help "出力先之指定 (指定無キ時、算譜之拡張子名ヲ変更為タ物ト成ル。)"
 				&=	groupname "換符ニ関為ル選項",
--}		verbose	=	OFF
+		verbose	=	OFF
 				&=	opt "OFF"
 				&=	typ " OFF | ON"
 				&=	help "換符処理之表示ヲ行フ"
@@ -66,11 +65,11 @@ cmdOpt = CmdOpt
 				"製作場: https://github.com/orgs/KCCTdensan/teams/team-1/members"
 			 ]
 
-machineIsSelected :: CmdOpt -> Bool
-machineIsSelected args = machine args /= NoSelM
+computerIsSelected :: CmdOpt -> Bool
+computerIsSelected args = computer args /= NoSelC
 
-directionIsSelected :: CmdOpt -> Bool
-directionIsSelected args = direction args /= NoSelD
+modeIsSelected :: CmdOpt -> Bool
+modeIsSelected args = mode args /= NoSelD
 
 main = do
 	args <- cmdArgs cmdOpt
@@ -78,10 +77,10 @@ main = do
 	-- 選項之検査 --------------------------------------------------------------
 	flag <- newIORef False
 
-	unless (machineIsSelected args) $ do
+	unless (computerIsSelected args) $ do
 		flag `writeIORef` True
 		putStrLn "錯誤: 対象之計算機が指定為れて居ません。"
-	unless (directionIsSelected args) $ do
+	unless (modeIsSelected args) $ do
 		flag `writeIORef` True
 		putStrLn "錯誤: 換符之方向が指定為れて居ません。"
 	exist <- doesFileExist $ file args
@@ -93,15 +92,16 @@ main = do
 	when err $ do
 		exitWith $ ExitFailure (-1)
 	-----------------------------------------------------------------------------
-{-
-	inputFile <- head <$> getArgs
 
+	-- 換符処理 -----------------------------------------------------------------
+	-- [要修正]: 選項之反映
+	let inputFile = file args
 	let outputFile = head (splitRegex (mkRegex "[.]+") inputFile) ++ ".bin"
 	ops <- lines <$> readFile inputFile
-	writeFile outputFile (unlines $ assemble (machine args) ops) -- [要修正] 例外処理追加
+	writeFile outputFile (unlines $ assemble (computer args) ops) -- [要修正] 例外処理之追加
+	-----------------------------------------------------------------------------
 
-assemble :: Machine -> [String] -> [String]
+assemble :: Computer -> [String] -> [String]
 assemble TD4 ops = map TD4.assemble $ map (map toLower) ops
 assemble Type1 ops =  error "非対応な計算機: type1"
 assemble Type2 ops = error "非対応な計算機: type2"
--}
--- README.md
+++ README.md
@@ -10,5 +10,5 @@ Assembler
     $ ghc Main.hs -o asm
 
 ## 実行例
-    $ asm -m=TD4 -d=asm Flash-LED.asm
-    $ asm --machine=Type1 --direction=DisASM --verbose=ON Fibonacci.bin --output Fib.asm 
+    $ asm -c=TD4 -d=asm Flash-LED.asm
+    $ asm --computer=Type1 --mode=DisASM --verbose=ON Fibonacci.bin --output Fib.asm 
